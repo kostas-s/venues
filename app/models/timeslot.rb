68dc41f245751f99e4eb34f5247370daeeb23f6b
kostas-s <65230255+kostas-s@users.noreply.github.com>
2021-09-20 12:59:19 +0300

    latest heroku push

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -5,4 +5,6 @@ class Timeslot < ApplicationRecord
   validates :day, inclusion: { in: VALID_DAYS }
   validates :start_time, presence: true
   validates :end_time, presence: true
+
+  enum day: { mon: 1, tue: 2, wed: 3, thu: 4, fri: 5, sat: 6, sun: 7 }
 end
